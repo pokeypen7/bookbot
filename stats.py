6cd096c3b1a63de45e1078cce7c63b235e81ec6f
pokeypen7 <bryanbadilla77@gmail.com>
2025-12-11 13:26:29 -0800

    reworking stats.py

--- stats.py
+++ stats.py
@@ -10,15 +10,16 @@ def get_num_words(file_string):
     return counted_text
 
 def create_char_dic(file_string):
-   char_ct = {}
-   for char in file_string:
-       if char.lower() not in char_ct:
-           char_ct[char.lower()] = 0
-       char_ct[char.lower()] += 1
-   return char_ct
+    char_ct = {}
+    for char in file_string:
+        char_low = char.lower()
+        if char_low not in char_ct:
+            char_ct[char_low] = 0
+            char_ct[char_low] += 1
+    return char_ct
 
-def sort_on(items):
-    return 
-
-def sort_dic(char_dic):
-    char_dic.sort(reverse=True, key)
+# def sort_on(items):
+#     return 
+# 
+# def sort_dic(char_dic):
+#     char_dic.sort(reverse=True, key)
